55015dd1f7a8e4e34124acf24a6bc76ec41a4c8d
theeightboys <theboysofficialone@gmail.com>
2026-04-23 21:33:29 +0530

    added supabase

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: '',
-  publishableKey: ''
+  url: 'https://onxkuhbjryritxcozdwk.supabase.co',
+  publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueGt1aGJqcnlyaXR4Y296ZHdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzA2OTUsImV4cCI6MjA5MTU0NjY5NX0.b0AM70Sea-ipo0dvjHSqRHvCu0hWaPzRVRnXw4Y5JVY'
 };
